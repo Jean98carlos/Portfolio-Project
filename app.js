cf3436a1ac8311648f53360f807dcb52b02e8fb7
Jean98carlos <jecarlos.me@gmail.com>
2020-02-26 21:14:19 -0300

    Finalizadas animações

--- app.js
+++ app.js
@@ -10,11 +10,13 @@ function init(){
     //Page tracker
 
     let current = 0;
+    let scrollSlide = 0;
     
     slides.forEach((slide,index) => {
         slide.addEventListener("click", function(){
             changeDots(this);
             nextSlide(index);
+            scrollSlide = index;
         });
     });
 
@@ -54,8 +56,65 @@ function init(){
             current = pageNumber;
         }
     }
+    document.addEventListener("wheel", throttle(scrollChange, 1500));
+    document.addEventListener("touchmove", throttle(scrollChange, 1500));
 
-    
+    function switchDots(dotNumber) {
+        const activeDot = document.querySelectorAll(".slide")[dotNumber];
+        slides.forEach(slide => {
+            slide.classList.remove("active");
+        });
+        activeDot.classList.add("active");
+    }
+
+    function scrollChange(e) {
+        if (e.deltaY > 0) {
+            scrollSlide += 1;
+        }else{
+            scrollSlide -= 1;
+        }
+
+        if (scrollSlide > 2) {
+            scrollSlide = 0;
+        }
+        if (scrollSlide < 0) {
+            scrollSlide = 2;
+        }
+        switchDots(scrollSlide);
+        nextSlide(scrollSlide);
+    }
+
+    const hamburger = document.querySelector(".menu");
+    const hamburgerLines = document.querySelectorAll(".menu line");
+    const navOpen = document.querySelector(".nav-open");
+    const contact = document.querySelector(".contact");
+    const social = document.querySelector(".social");
+    const logo = document.querySelector(".logo");
+
+    const tl = new TimelineMax({ paused: true, reversed: true });
+
+    tl.to(navOpen, 0.5, { y: 0 })
+    .fromTo(contact, 0.5, { opacity: 0, y: 10 }, { opacity: 1, y: 0 }, "-=0.1")
+    .fromTo(social, 0.5, { opacity: 0, y: 10 }, { opacity: 1, y: 0 }, "-=0.5")
+    .fromTo(logo, 0.2, { color: "white" }, { color: "black" }, "-=1")
+    .fromTo(hamburgerLines, 0.2,{ stroke: "white" },{ stroke: "black" },"-=1");
+
+    hamburger.addEventListener("click", () => {
+        tl.reversed() ? tl.play() : tl.reverse();
+    });
+}
+
+function throttle(func, limit) {
+    let inThrottle;
+    return function() {
+    const args = arguments;
+    const context = this;
+    if (!inThrottle) {
+        func.apply(context, args);
+        inThrottle = true;
+        setTimeout(() => (inThrottle = false), limit);
+    }
+    };
 }
 
 init();
